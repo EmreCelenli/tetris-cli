b47dd6bf137a120152052939f402e035deddb2bd
simoPTZ <150898339+simoPTZ@users.noreply.github.com>
2024-03-13 11:12:29 +0100

    Add files via upload

--- Grid.cpp
+++ Grid.cpp
@@ -6,8 +6,6 @@ Grid::Grid(WINDOW * win) : block(){
         for(int j = 0; j < 10; j++)
             grid[i][j] = false;
     }
-
-    addBlock();
 }
 
 void Grid::addBlock(){
@@ -20,11 +18,11 @@ void Grid::addBlock(){
 
 void Grid::displayGrid(){
     for(int i = 0; i < 20; i++){
-        for(int j = 0; j < 10; j++){
-            if(grid[i][j])
-                mvwaddch(win, i, j, '*');
+        for(int j = 0; j < 20; j += 2){
+            if(grid[i][j/2])
+                mvwprintw(win, i, j, "[]");
             else 
-                mvwaddch(win, i, j, ' ');
+                mvwprintw(win, i, j, " %c", 46);
         }
     }
     wrefresh(win);
@@ -44,27 +42,33 @@ bool Grid::checkDirection(int direction){
 
     removeBlock();
 
-    bool condition;    
+    bool blocksCondition, borderCondition;    
     bool isFree = true;
 
     for(int i = 0; i < 4; i++){
         switch(direction){
             case KEY_DOWN:
-                condition = grid[blockCoordinates[i].y + 1][blockCoordinates[i].x];
+                blocksCondition = grid[blockCoordinates[i].y + 1][blockCoordinates[i].x];
+                borderCondition = blockCoordinates[i].y + 1 > 19;
                 break;
             case KEY_RIGHT:
-                condition = grid[blockCoordinates[i].y][blockCoordinates[i].x + 1];
+                blocksCondition = grid[blockCoordinates[i].y][blockCoordinates[i].x + 1];
+                borderCondition = blockCoordinates[i].x + 1 > 9;
                 break;
             case KEY_LEFT:
-                condition = grid[blockCoordinates[i].y][blockCoordinates[i].x - 1];
+                blocksCondition = grid[blockCoordinates[i].y][blockCoordinates[i].x - 1];
+                borderCondition = blockCoordinates[i].x - 1 < 0;
                 break;
             default:
                 break;    
         }
 
-        if(condition) 
+        if(blocksCondition || borderCondition) 
             isFree = false;
     }
+
+    if(!isFree)
+        addBlock();
 
     return isFree;
 }
@@ -76,10 +80,109 @@ void Grid::moveDirection(int direction){
     displayGrid();
 }
 
-void Grid::moveDownDelayed(){
-    while(1){
+void Grid::moveDownDelayed(int delay = 10){
+    halfdelay(delay);
+    int command;
+
+    while(checkDirection(KEY_DOWN)){
+        command = wgetch(win);
+        if(command != ERR){ 
+            switch(command){
+                case 97: 
+                    mvwprintw(win, 0, 0, "A");
+                    rotation(false);
+                    break;
+                case 100:
+                    mvwprintw(win, 0, 0, "D");
+                    rotation(true);
+                    break;
+                default:
+                    moveDirection(command);
+                    break;
+            }
+                
+            wgetch(win);
+        }
         moveDirection(KEY_DOWN);
-        sleep(1);
     }
 }
 
+void Grid::game(){
+    bool startOccupied = false;
+    while(!startOccupied){
+        addBlock();
+        moveDownDelayed();
+        removeLines();
+
+        Tetramino newblock;
+        block = newblock;
+
+        startOccupied = checkBlockArea();        
+    }
+}
+
+int Grid::removeLines(){
+    int removed = 0;
+    int lowest_line = -1;
+    for(int i = 0; i < 20; i++){
+        if(checkCompleted(i)){
+            for(int j = 0; j < 10; j++)
+                grid[i][j] = false;
+                if(i > lowest_line)
+                    lowest_line = i;
+            removed++;
+        }
+    }
+
+    for(int i = 0; i < removed; i++){
+        for(int j = lowest_line; j > 0; j--){
+            for(int k = 0; k < 10; k++)
+                grid[j][k] = grid[j-1][k];
+        }
+    }
+
+    return removed;
+}
+
+bool Grid::checkCompleted(int line){ // controlla che una riga sia completa
+    bool isComplete = true;
+    for(int j = 0; j < 10; j++){
+        if(!grid[line][j])
+            isComplete = false;
+    }
+
+    return isComplete;
+}
+
+
+bool Grid::checkBlockArea(){
+    point blockCoordinates[4]; 
+    block.getCoordinates(blockCoordinates);
+
+    bool isOccupied = false;
+    for(int i = 0; i < 4; i++){
+        if(grid[blockCoordinates[i].y][blockCoordinates[i].x])
+            isOccupied = true;
+    }
+
+    return isOccupied;
+}
+
+void Grid::rotation(bool clockwise){ // 1 vuol dire clockwise 0 vuol dire counterclockwise
+    if(block.getID() != 1){
+        point blockCoordinates[4];
+        block.getCoordinates(blockCoordinates);
+        removeBlock();
+        block.rotate(clockwise);
+        
+        while(block.isOut() == -1)
+            block.move(KEY_RIGHT);
+        while(block.isOut() == 1)
+            block.move(KEY_LEFT);
+        while(checkBlockArea()){
+            block.move(KEY_UP);
+        }
+
+        addBlock();
+    }    
+}
--- Tetramino.cpp
+++ Tetramino.cpp
@@ -2,7 +2,7 @@
 
 Tetramino::Tetramino(){
     srand(time(0));
-    id = rand() % 2;
+    this->id = rand() % 7;
 
     for(int i = 0; i < 4; i++)
         block[i] = defaultMatrix[id][i];
@@ -27,7 +27,73 @@ void Tetramino::move(int direction){
             for(int i = 0; i < 4; i++)
                 block[i].x--;
             break;
+        case KEY_UP:
+            for(int i = 0; i < 4; i++)
+                block[i].y--;
+            break;
         default:
             break;
     }
+}
+
+void Tetramino::convertCoordinates(point relativeCoordinates[], point gridCoordinates[]){
+    for(int i = 0; i < 4; i++){
+        gridCoordinates[i].y = relativeCoordinates[i].y + block[0].y;
+        gridCoordinates[i].x = relativeCoordinates[i].x + block[0].x;
+    }
+}
+
+int Tetramino::getID(){
+    return this->id;
+}
+
+void Tetramino::rotate(bool clockwise){
+    if(this->id != 1){
+        point relativeCoordinates[4];
+        for(int i = 0; i < 4; i++){
+            relativeCoordinates[i].y = block[i].y - block[0].y;
+            relativeCoordinates[i].x = block[i].x - block[0].x;
+        }
+
+        int prev_x;
+
+        if(!clockwise){
+            for(int i = 0; i < 4; i++){
+                prev_x = relativeCoordinates[i].x;
+                relativeCoordinates[i].x = relativeCoordinates[i].y;
+                relativeCoordinates[i].y = -prev_x;
+            }
+        }
+        else {
+            for(int j = 0; j < 3; j++){
+                for(int i = 0; i < 4; i++){
+                    prev_x = relativeCoordinates[i].x;
+                    relativeCoordinates[i].x = relativeCoordinates[i].y;
+                    relativeCoordinates[i].y = -prev_x;
+                }            
+            }
+
+        }       
+
+        point gridCoordinates[4];
+        convertCoordinates(relativeCoordinates, gridCoordinates);
+        insert(gridCoordinates); 
+    }
+}
+
+void Tetramino::insert(point gridCoordinates[]){
+    for(int i = 0; i < 4; i++)
+        block[i] = gridCoordinates[i];
+}
+
+int Tetramino::isOut(){
+    int isOut = 0;
+    for(int i = 0; i < 4; i++){
+        if(block[i].x > 9)
+            isOut = 1;
+        else if(block[i].x < 0)
+            isOut = -1;
+    }
+
+    return isOut;
 }
